5f01d823d1746712203fe8ad0ab4dd09c3f54a56
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-22 12:19:50 +0000

    Localisation updates for extensions from Betawiki (2007-12-22 12:25 CET)

--- SpecialInterwiki.i18n.php
+++ SpecialInterwiki.i18n.php
@@ -241,11 +241,6 @@ $wgSpecialInterwikiMessages['la'] = array(
 	'interwiki_defaultreason' => 'nulla causa data',
 );
 
-/** Burmese (Myanmasa) */
-$wgSpecialInterwikiMessages['my'] = array(
-	'interwiki_reasonfield' => 'કારણ',
-);
-
 /** Dutch (Nederlands)
  * @author SPQRobin
  */
