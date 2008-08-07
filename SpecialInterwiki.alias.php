4ff6147ad5f4f614f92680b6c9f27964d8e78719
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-07 23:35:37 +0000

    Fix another path to special page aliases file, and add some consistency

--- SpecialInterwiki.alias.php
+++ SpecialInterwiki.alias.php
@@ -11,12 +11,12 @@ $aliases = array();
  * @author Jon Harald Søby
  */
 $aliases['en'] = array(
-	'Interwiki'            => array('Interwiki')
+	'Interwiki' => array('Interwiki'),
 );
 
 /** Norwegian (Norsk (bokmål))
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
-	'Interwiki'            => array('Interwiki')
+	'Interwiki' => array('Interwiki'),
 );
