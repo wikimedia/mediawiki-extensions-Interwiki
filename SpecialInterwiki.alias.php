4f1dd5587fc7195fa155e7c7d8b8d3df08385b6c
Meno25 <mfarag@users.mediawiki.org>
2008-08-08 19:23:24 +0000

    Add author info

--- SpecialInterwiki.alias.php
+++ SpecialInterwiki.alias.php
@@ -14,12 +14,16 @@ $aliases['en'] = array(
 	'Interwiki' => array('Interwiki'),
 );
 
-/** Arabic (العربية) */
+/** Arabic (العربية)
+ * @author Meno25
+ */
 $aliases['ar'] = array(
 	'Interwiki' => array( 'إنترويكي' ),
 );
 
-/** Egyptian Spoken Arabic (مصرى) */
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
 $aliases['arz'] = array(
 	'Interwiki' => array( 'إنترويكى' ),
 );
