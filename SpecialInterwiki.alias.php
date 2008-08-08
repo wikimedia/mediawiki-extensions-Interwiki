431ed9b1fab8bb143fea68fcd2cdb6b8ac0bd9df
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-08 09:37:07 +0000

    Localisation updates for extension special page aliases from Betawiki

--- SpecialInterwiki.alias.php
+++ SpecialInterwiki.alias.php
@@ -14,9 +14,20 @@ $aliases['en'] = array(
 	'Interwiki' => array('Interwiki'),
 );
 
-/** Norwegian (Norsk (bokmål))
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'Interwiki' => array( 'إنترويكي' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'Interwiki' => array( 'إنترويكى' ),
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
-	'Interwiki' => array('Interwiki'),
+	'Interwiki' => array( 'Interwiki' ),
 );
+
