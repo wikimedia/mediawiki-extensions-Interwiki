2d6236edfc9e5090125b4ab2b118cd111ba11eac
Umherirrender <umherirrender_de.wp@web.de>
2017-12-16 14:14:00 +0100

    Move InterwikiLogFormatter to own file
    Makes MediaWiki.Files.OneClassPerFile.MultipleFound pass
    
    Change-Id: I463de4435b1f1ef82b9e87b35ebc3ad16e181898

--- Interwiki_body.php
+++ Interwiki_body.php
@@ -496,19 +496,3 @@ class SpecialInterwiki extends SpecialPage {
 		return 'wiki';
 	}
 }
-
-/**
- * Needed to pass the URL as a raw parameter, because it contains $1
- */
-class InterwikiLogFormatter extends LogFormatter {
-	/**
-	 * @return array
-	 */
-	protected function getMessageParameters() {
-		$params = parent::getMessageParameters();
-		if ( isset( $params[4] ) ) {
-			$params[4] = Message::rawParam( htmlspecialchars( $params[4] ) );
-		}
-		return $params;
-	}
-}
